5d427636fbbcc0aff0bd857752db36f04e9f0db0
Igor D <igor.datsenko@hotmail.com>
2026-01-30 00:43:04 +0100

    Init BE application

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+ async function start() {
+    await connectDB();
+    await app.listen(port, () =>
+     console.log(`Server is running on port ${port}`)
+    );
+ }
+
+async function connectDB() {
+    const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zvcetbr.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`
+
+    await mongoose.connect(url, {})
+}
+
+start();
